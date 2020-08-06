1130bf025e96e68188ae1870e1a261fa42bb1236
Heloisa <halvesmorais@gmail.com>
2020-08-05 22:02:05 -0300

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { FormularioComponent } from './formulario/formulario.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: FormularioComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
